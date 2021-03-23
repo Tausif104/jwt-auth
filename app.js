857e266bfc72b7868c4871485800842582b3a79a
tausif104 <tausifahmed49@gmail.com>
2021-03-24 03:28:13 +0600

    initial commit

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const mongoURI = require('./config/db')
+
+const app = express()
+
+// middleware
+app.use(express.static('public'))
+
+// view engine
+app.set('view engine', 'ejs')
+
+mongoose
+    .connect(mongoURI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    })
+    .then((res) => console.log(`MongoDB Connected`))
+    .catch((error) => console.error(error))
+
+const PORT = process.env.PORT || 5000
+
+// routes
+app.get('/', (req, res) => res.render('home'))
+app.get('/smoothies', (req, res) => res.render('smoothies'))
+
+app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
---
+++ config/db.js
@@ -0,0 +1,4 @@
+const mongoURI =
+    'mongodb+srv://tausif104:tausif104@jwt.7qn4r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+module.exports = mongoURI
